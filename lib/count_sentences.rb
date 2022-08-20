c6e6c53888197ebe0d7fbde8f9fe411b8f21ce1e
Mwenje <mwenje24@gmail.com>
2022-08-20 11:21:27 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "Self is: #{self}".end_with?(".")
   end
 
   def question?
-
+    "Self is: #{self}".end_with?("?")
   end
 
   def exclamation?
-
+    "Self is: #{self}".end_with?("!")
   end
 
   def count_sentences
-
+    "#{self}".split(/\.|\?|\!/).compact.filter {|elem| elem != ""}.length
   end
 end
